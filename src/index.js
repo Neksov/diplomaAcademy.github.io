7d0cbd512770500248010d80b7acc5cf91fff557
Nekrasov Vadim <n2v@list.ru>
2020-11-26 21:27:47 +0300

    feat: Установка webpack

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 'use strict';
 
+import 'nodelist-foreach-polyfill';
+import "@babel/polyfill";
+import elementClosest from  'element-closest';
+elementClosest(window);
+import 'formdata-polyfill';
+import 'es6-promise';
+import 'fetch-polyfill';
+import 'regexp-polyfill';
+import smoothscroll from 'smoothscroll-polyfill';
+smoothscroll.polyfill();
+import browser from 'cross-browser-polyfill';
+browser();
+
 import togglePopUp from './modules/togglePopUp';
 import showAll from './modules/showAll';
 import tabs from './modules/tabs';
